49da7d6234edeb51e5a9dd9ffbc205b33b1640f6
Simon Rofe <simon.rofe@ieee.org>
2019-07-22 17:51:24 +1000

    Initial project setup with failing test

---
+++ Segues/SegueViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SegueViewController.swift
+//  Segues
+//
+//  Created by Simon Rofe on 22/7/19.
+//  Copyright © 2019 Simon Rofe. All rights reserved.
+//
+
+import UIKit
+
+class SegueViewController: UIViewController {
+}
---
+++ SeguesTests/SegueViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  SegueViewControllerTests.swift
+//  SeguesTests
+//
+//  Created by Simon Rofe on 22/7/19.
+//  Copyright © 2019 Simon Rofe. All rights reserved.
+//
+
+import XCTest
+
+class SegueViewControllerTests: XCTestCase {
+
+    func test_zero() {
+        XCTFail("Initial failing test for SegueViewController.")
+    }
+}
